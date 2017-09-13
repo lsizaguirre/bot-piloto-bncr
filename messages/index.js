1e4083e15e658336e1150d78bfc35b1712953100
Luis Izaguirre <lsizaguirre@gmail.com>
2017-09-13 19:05:24 -0300

    Fix Azure

--- messages/index.js
+++ messages/index.js
@@ -41,31 +41,6 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] } )
     session.send(name + ' ' + args.entities[0].entity);
 });
 
-// Add dialog to return list of shirts available
-bot.dialog('showShirts', function (session) {
-    var msg = new builder.Message(session);
-    msg.attachmentLayout(builder.AttachmentLayout.carousel)
-    msg.attachments([
-        new builder.HeroCard(session)
-            .title("Classic White T-Shirt")
-            .subtitle("100% Soft and Luxurious Cotton")
-            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
-            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
-            .buttons([
-                builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
-            ]),
-        new builder.HeroCard(session)
-            .title("Classic Gray T-Shirt")
-            .subtitle("100% Soft and Luxurious Cotton")
-            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
-            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
-            .buttons([
-                builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
-            ])
-    ]);
-    session.send(msg).endDialog();
-}).triggerAction({ matches: /^(show|list)/i });
-
 bot.dialog('/', intents);    
 
 // Set the Incoming and Outcoming functions for the middleware

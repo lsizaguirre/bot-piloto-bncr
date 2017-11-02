0d4c9125b65622ceeb6909c4affef35038bf0521
Luis Izaguirre <lsizaguirre@gmail.com>
2017-11-02 13:35:43 -0300

    Prueba trigger events 1

--- libs/bot-utils/index.js
+++ libs/bot-utils/index.js
@@ -27,6 +27,7 @@ const buildBot = connector => {
     bot.localePath(path.join(__dirname, './locale'));
     bot.library(locationDialog.createLibrary(process.env.BING_MAPS_API_KEY));
     dialogs.setDialogs(bot);
+    this.appbot = bot;
     return bot;
 }
 
@@ -37,7 +38,29 @@ const startLocalServer = connector => {
     server.listen(3978, function() {
         console.log('test bot endpoint at http://localhost:3978/api/messages');
     });
-    server.post('/api/messages', connector.listen());    
+    server.post('/api/messages', connector.listen());
+
+
+
+    server.post('/event/post', (req, res, next) => {
+
+        const builder = require("botbuilder");
+        const botbuilder_azure = require("botbuilder-azure");
+        
+        this.appbot.loadSession(req.body.address, function(err, session){
+            if(err) {
+                console.log(err);
+            }
+            else {
+                var msg = new builder.Message().address(req.body.address);
+                msg.text('Work!');
+                msg.textLocale('en-US');
+                session.send(msg);
+            }
+        });
+        next();
+        res.json(true);
+    });
 }
 
 module.exports = {
